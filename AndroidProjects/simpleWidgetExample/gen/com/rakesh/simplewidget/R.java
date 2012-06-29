bce83818cfdd6987b15658cf7a7173eea04682d0
Rakesh Kumar <rakeshcusat@gmail.com>
2012-06-28 22:02:17 -0700

    Added icon and did some little tweak.

--- AndroidProjects/simpleWidgetExample/gen/com/rakesh/simplewidget/R.java
+++ AndroidProjects/simpleWidgetExample/gen/com/rakesh/simplewidget/R.java
@@ -14,6 +14,7 @@ public final class R {
         public static final int background=0x7f020000;
         public static final int button_shape=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int widget_icon=0x7f020003;
     }
     public static final class id {
         public static final int BtEnableDisable=0x7f060001;
