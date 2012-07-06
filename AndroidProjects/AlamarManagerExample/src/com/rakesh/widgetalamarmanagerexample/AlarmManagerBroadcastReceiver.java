ea37fad3bac9cfdac7c4d280dcc1d791c4429c15
Rakesh Kumar <rakeshcusat@gmail.com>
2012-07-05 23:16:15 -0700

    Renamed the project

---
+++ AndroidProjects/AlamarManagerExample/src/com/rakesh/widgetalamarmanagerexample/AlarmManagerBroadcastReceiver.java
@@ -0,0 +1,48 @@
+package com.rakesh.widgetalamarmanagerexample;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager;
+import android.widget.Toast;
+
+public class AlarmManagerBroadcastReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		 PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+         PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "YOUR TAG");
+         //Acquire the lock
+         wl.acquire();
+
+         //You can do the processing here update the widget/remote views.
+         Format formatter = new SimpleDateFormat("hh:mm:ss a");
+         String time = formatter.format(new Date());
+         Toast.makeText(context, time, Toast.LENGTH_LONG).show();
+         //Release the lock
+         wl.release();
+         
+	}
+	public void SetAlarm(Context context)
+    {
+        AlarmManager am=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent(context, AlarmManagerBroadcastReceiver.class);
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
+        //After after 30 seconds
+        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 5 , pi); 
+    }
+
+    public void CancelAlarm(Context context)
+    {
+        Intent intent = new Intent(context, AlarmManagerBroadcastReceiver.class);
+        PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(sender);
+    }
+}
