5826169f5905420ce0321cf73250d3d3ec5abad1
Rakesh Kumar <rakeshcusat@gmail.com>
2012-07-15 23:41:40 -0700

    Did some minor changes in JellyBEanNotificationExample project.

---
+++ AndroidProjects/JellyBeanNotificationExample/src/com/example/jellybeannotificationexample/HandleNotificationActivity.java
@@ -0,0 +1,14 @@
+package com.example.jellybeannotificationexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HandleNotificationActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.handle_notification_activity);
+	}
+
+}
