089a33229f77c9426dfc0e30537911dff1380dac
Rakesh Kumar <rakeshcusat@gmail.com>
2012-07-23 22:00:09 -0700

    Added some more features in Voice recognition example

--- AndroidProjects/VoiceRecognitionExample/gen/com/rakesh/voicerecognitionexample/R.java
+++ AndroidProjects/VoiceRecognitionExample/gen/com/rakesh/voicerecognitionexample/R.java
@@ -8,6 +8,9 @@
 package com.rakesh.voicerecognitionexample;
 
 public final class R {
+    public static final class array {
+        public static final int saNoOfMatches=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -20,24 +23,28 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int etTextHint=0x7f080000;
-        public static final int lvTextMatches=0x7f080001;
-        public static final int menu_settings=0x7f080002;
+        public static final int btSpeak=0x7f090001;
+        public static final int etTextHint=0x7f090000;
+        public static final int lvTextMatches=0x7f090003;
+        public static final int menu_settings=0x7f090004;
+        public static final int sNoOfMatches=0x7f090002;
     }
     public static final class layout {
         public static final int activity_voice_recognition=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_voice_recognition=0x7f070000;
+        public static final int activity_voice_recognition=0x7f080000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int btSpeak=0x7f050001;
+        public static final int etSearchHint=0x7f050006;
         public static final int menu_settings=0x7f050002;
+        public static final int sNoOfMatches=0x7f050005;
         public static final int title_activity_voice_recognition=0x7f050003;
         public static final int tvTextMatches=0x7f050004;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
     }
 }
--- AndroidProjects/VoiceRecognitionExample/src/com/rakesh/voicerecognitionexample/VoiceRecognitionActivity.java
+++ AndroidProjects/VoiceRecognitionExample/src/com/rakesh/voicerecognitionexample/VoiceRecognitionActivity.java
@@ -1,60 +1,130 @@
 package com.rakesh.voicerecognitionexample;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
+import android.app.SearchManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class VoiceRecognitionActivity extends Activity {
- private static final int VOICE_RECOGNITION_REQUEST_CODE = 1001;
- private EditText metTextHint;
- private ListView mlvTextMatches;
- 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_voice_recognition);
-        metTextHint = (EditText)findViewById(R.id.etTextHint);
-        mlvTextMatches = (ListView)findViewById(R.id.lvTextMatches);
-    }
+	private static final int VOICE_RECOGNITION_REQUEST_CODE = 1001;
 
-    public void speak(View view){
-    	Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+	private EditText metTextHint;
+	private ListView mlvTextMatches;
+	private Spinner msTextMatches;
+	private Button mbtSpeak;
 
-        // Specify the calling package to identify your application
-        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass().getPackage().getName());
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_voice_recognition);
+		metTextHint = (EditText) findViewById(R.id.etTextHint);
+		mlvTextMatches = (ListView) findViewById(R.id.lvTextMatches);
+		msTextMatches = (Spinner) findViewById(R.id.sNoOfMatches);
+		mbtSpeak = (Button) findViewById(R.id.btSpeak);
+	}
 
-        // Display an hint to the user about what he should say.
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speech recognition demo");
+	public void checkVoiceRecognition() {
+		// Check to see if a recognition activity is present
+		PackageManager pm = getPackageManager();
+		List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(
+				RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+		if (activities.size() == 0) {
+			mbtSpeak.setEnabled(false);
+			Toast.makeText(this, "Voice recognizer not present",
+					Toast.LENGTH_SHORT).show();
+		}
+	}
 
-        // Given an hint to the recognizer about what the user is going to say
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+	public void speak(View view) {
+		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
 
-        // Specify how many results you want to receive. The results will be sorted
-        // where the first result is the one with higher confidence.
-        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
+		// Specify the calling package to identify your application
+		intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass()
+				.getPackage().getName());
 
+		// Display an hint to the user about what he should say.
+		intent.putExtra(RecognizerIntent.EXTRA_PROMPT, metTextHint.getText()
+				.toString());
 
-        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
-            // Fill the list view with the strings the recognizer thought it could have heard
-            ArrayList<String> textMatcheList = data.getStringArrayListExtra(
-                    RecognizerIntent.EXTRA_RESULTS);
-            mlvTextMatches.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-                    textMatcheList));
-        }
+		// Given an hint to the recognizer about what the user is going to say
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+				RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
 
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-    
+		// If number of Matches is not selected then return show toast message
+		if (msTextMatches.getSelectedItemPosition() == AdapterView.INVALID_POSITION) {
+			Toast.makeText(this, "Please select No. of Matches from spinner",
+					Toast.LENGTH_SHORT).show();
+			return;
+		}
+
+		int noOfMatches = Integer.parseInt(msTextMatches.getSelectedItem()
+				.toString());
+		// Specify how many results you want to receive. The results will be
+		// sorted where the first result is the one with higher confidence.
+
+		intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, noOfMatches);
+
+		startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == VOICE_RECOGNITION_REQUEST_CODE)
+
+			//If Voice recognition is successful then it returns RESULT_OK
+			if(resultCode == RESULT_OK) {
+
+				ArrayList<String> textMatchList = data
+				.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+				if (!textMatchList.isEmpty()) {
+					// If first Match contains the 'search' word
+					// Then start web search.
+					if (textMatchList.get(0).contains("search")) {
+
+						String searchQuery = textMatchList.get(0).replace("search",
+						" ");
+						Intent search = new Intent(Intent.ACTION_WEB_SEARCH);
+						search.putExtra(SearchManager.QUERY, searchQuery);
+						startActivity(search);
+					} else {
+						// populate the Matches
+						mlvTextMatches
+						.setAdapter(new ArrayAdapter<String>(this,
+								android.R.layout.simple_list_item_1,
+								textMatchList));
+					}
+
+				}
+			//Result code for various error.	
+			}else if(resultCode == RecognizerIntent.RESULT_AUDIO_ERROR){
+				showToastMessage("Audio Error");
+			}else if(resultCode == RecognizerIntent.RESULT_CLIENT_ERROR){
+				showToastMessage("Client Error");
+			}else if(resultCode == RecognizerIntent.RESULT_NETWORK_ERROR){
+				showToastMessage("Network Error");
+			}else if(resultCode == RecognizerIntent.RESULT_NO_MATCH){
+				showToastMessage("No Match");
+			}else if(resultCode == RecognizerIntent.RESULT_SERVER_ERROR){
+				showToastMessage("Server Error");
+			}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+	void showToastMessage(String message){
+		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+	}
 }
