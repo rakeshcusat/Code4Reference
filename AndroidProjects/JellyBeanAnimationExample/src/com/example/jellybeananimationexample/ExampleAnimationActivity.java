0de2d1c034a6160458f7dca6a22d17cfd5a99ded
Rakesh Kumar <rakeshcusat@gmail.com>
2012-07-31 22:30:31 -0700

    Added new JellyBeanAnimationExample project

---
+++ AndroidProjects/JellyBeanAnimationExample/src/com/example/jellybeananimationexample/ExampleAnimationActivity.java
@@ -0,0 +1,13 @@
+package com.example.jellybeananimationexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExampleAnimationActivity extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation);
+    }
+}
---
+++ AndroidProjects/JellyBeanAnimationExample/src/com/example/jellybeananimationexample/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.jellybeananimationexample;
+
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void scaleupAnimation(View view){
+		 // Create a scale-up animation that originates at the button
+        // being pressed.
+        ActivityOptions opts = ActivityOptions.makeScaleUpAnimation(
+                view, 0, 0, view.getWidth(), view.getHeight());
+        // Request the activity be started, using the custom animation options.
+        startActivity(new Intent(MainActivity.this, ExampleAnimationActivity.class), opts.toBundle());
+	}
+	
+	public void thumbNailScaleAnimation(View view){
+	    view.setDrawingCacheEnabled(true);
+        view.setPressed(false);
+        view.refreshDrawableState();
+        Bitmap bitmap = view.getDrawingCache();
+        Canvas canvas = new Canvas(bitmap);
+        ActivityOptions opts = ActivityOptions.makeThumbnailScaleUpAnimation(
+                view, bitmap, 0, 0);
+        // Request the activity be started, using the custom animation options.
+        startActivity(new Intent(MainActivity.this, ExampleAnimationActivity.class), opts.toBundle());
+        view.setDrawingCacheEnabled(false);
+	}
+	
+
+}
