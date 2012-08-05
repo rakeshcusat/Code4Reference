4e667645fd6117f0dcc5b9d2f34f41b581d3ae4b
Rakesh Kumar <rakeshcusat@gmail.com>
2012-08-05 15:27:44 -0700

    Added new project folder GradleExample and modifed code of AndroidJelly bean notification.

--- AndroidProjects/JellyBeanAnimationExample/src/com/example/jellybeananimationexample/MainActivity.java
+++ AndroidProjects/JellyBeanAnimationExample/src/com/example/jellybeananimationexample/MainActivity.java
@@ -2,9 +2,6 @@ package com.example.jellybeananimationexample;
 
 import android.app.Activity;
 import android.app.ActivityOptions;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -18,39 +15,35 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
-	public void scaleupAnimation(View view){
+	public void scaleupAnimation(View view) {
 		// Create a scale-up animation that originates at the button
-        // being pressed.
-        ActivityOptions opts = ActivityOptions.makeScaleUpAnimation(
-                view, 0, 0, view.getWidth(), view.getHeight());
-        // Request the activity be started, using the custom animation options.
-        startActivity(new Intent(MainActivity.this, ExampleAnimationActivity.class), opts.toBundle());
+		// being pressed.
+		ActivityOptions opts = ActivityOptions.makeScaleUpAnimation(view, 0, 0,
+				view.getWidth(), view.getHeight());
+		// Request the activity be started, using the custom animation options.
+		startActivity(new Intent(MainActivity.this, AnimationActivity.class),
+				opts.toBundle());
 	}
-	
-	public void thumbNailScaleAnimation(View view){
-	    view.setDrawingCacheEnabled(true);
-        view.setPressed(false);
-        view.refreshDrawableState();
-        Bitmap bitmap = view.getDrawingCache();
-        ActivityOptions opts = ActivityOptions.makeThumbnailScaleUpAnimation(
-                view, bitmap, 0, 0);
-        // Request the activity be started, using the custom animation options.
-        startActivity(new Intent(MainActivity.this, ExampleAnimationActivity.class), opts.toBundle());
-        view.setDrawingCacheEnabled(false);
+
+	public void thumbNailScaleAnimation(View view) {
+		view.setDrawingCacheEnabled(true);
+		view.setPressed(false);
+		view.refreshDrawableState();
+		Bitmap bitmap = view.getDrawingCache();
+		ActivityOptions opts = ActivityOptions.makeThumbnailScaleUpAnimation(
+				view, bitmap, 0, 0);
+		// Request the activity be started, using the custom animation options.
+		startActivity(new Intent(MainActivity.this, AnimationActivity.class),
+				opts.toBundle());
+		view.setDrawingCacheEnabled(false);
 	}
-	
-	public void zoomAnimation(View view){
-		 ActivityOptions opts = ActivityOptions.makeCustomAnimation(MainActivity.this,
-                 R.anim.zoom_enter, R.anim.zoom_enter);
-         // Request the activity be started, using the custom animation options.
-         startActivity(new Intent(MainActivity.this, ExampleAnimationActivity.class), opts.toBundle());
+
+	public void fadeAnimation(View view) {
+		ActivityOptions opts = ActivityOptions.makeCustomAnimation(
+				MainActivity.this, R.anim.fade_in, R.anim.fade_out);
+		// Request the activity be started, using the custom animation options.
+		startActivity(new Intent(MainActivity.this, AnimationActivity.class),
+				opts.toBundle());
 	}
-	
-	public void fadeAnimation(View view){
-		 ActivityOptions opts = ActivityOptions.makeCustomAnimation(MainActivity.this,
-                 R.anim.fade_in, R.anim.fade_out);
-         // Request the activity be started, using the custom animation options.
-         startActivity(new Intent(MainActivity.this, ExampleAnimationActivity.class), opts.toBundle());
-	}
-	
+
 }
