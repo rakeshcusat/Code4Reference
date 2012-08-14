72f7d7348524e787475be42f817a15734d7621ce
Rakesh Kumar <rakeshcusat@gmail.com>
2012-08-14 08:41:07 -0700

    gradle standalone custom plugin done.

--- GradleExample/custom-plugin/plugin/src/main/groovy/com/code4reference/gradle/Code4ReferencePlugin.groovy
+++ GradleExample/custom-plugin/plugin/src/main/groovy/com/code4reference/gradle/Code4ReferencePlugin.groovy
@@ -2,9 +2,9 @@ package com.code4reference.gradle;
 
 import org.gradle.api.*;
 
-class C4r implements Plugin<Project> {
+class Code4ReferencePlugin implements Plugin<Project> {
     def void apply(Project project) {
-        project.task('demo') << {
+        project.task('c4rTask') << {
             println "Hi from Code4Reference plugin!"
         }
     }
