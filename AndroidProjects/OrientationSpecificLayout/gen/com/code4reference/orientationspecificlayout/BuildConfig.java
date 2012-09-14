38fef6e298374472e497b6b77fea48b3fda6fbf1
Rakesh Kumar <rakeshcusat@gmail.com>
2012-09-13 21:06:59 -0700

    Added OrientationSpecificLayout android project

---
+++ AndroidProjects/OrientationSpecificLayout/gen/com/code4reference/orientationspecificlayout/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.code4reference.orientationspecificlayout;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
